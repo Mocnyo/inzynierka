b652720ab5e30bbef3da3350c25690b2522d29ac
Dominik Jaruga <bob9732@gmail.com>
2019-09-23 23:22:34 +0200

    Init project

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $user = $this->getUser();
+
+         return $user != null ? $this->redirectToRoute('fos_user_profile_show') : $this->redirectToRoute('fos_user_security_login');
+    }
+}
